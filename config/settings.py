0b372678a37aa4319e6f3ca414750134d441b442
Hiko <xikomiqaelyan@gmail.com>
2024-11-21 22:16:04 +0100

    Added Added pictures and scaling added product cards and adding to cart

--- config/settings.py
+++ config/settings.py
@@ -117,13 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    ]
+STATIC_URL = '/static/'  # URL для доступа к статическим файлам
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Пути к папкам с исходными статическими файлами
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")   # Путь для команды collectstatic
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
